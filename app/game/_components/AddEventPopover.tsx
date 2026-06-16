aee86d1d3fb5f8750c87d6be23cb728d2ce29d94
rnegash <rnegash@gmail.com>
2026-06-16 16:02:20 +0200

    control eventpopover open/close

--- app/game/_components/AddEventPopover.tsx
+++ app/game/_components/AddEventPopover.tsx
@@ -1,7 +1,7 @@
 import { Popover, Button, Form } from "@heroui/react";
 import { Event, GameEvent } from "../types";
 import { handleFormSubmit } from "../_utils/handleFormSubmit";
-import { Dispatch, FormEvent, SetStateAction } from "react";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import { EventButton } from "../ClientGame";
 import { EventFormElements } from "./EventFormElements";
 
@@ -15,37 +15,48 @@ export const AddEventPopover = ({
   events: GameEvent[];
   setEvents: Dispatch<SetStateAction<GameEvent[]>>;
   players: { name: string }[];
-}) => (
-  <Popover>
-    <Button className={event.color} fullWidth size="lg">
-      {event.title}
-    </Button>
-    <Popover.Content className="max-w-72 overflow-scroll">
-      <Popover.Dialog>
-        <Popover.Heading className=" font-bold">
-          {event.addEvent}
-        </Popover.Heading>
-        <Form
-          className="flex flex-col gap-4"
-          onSubmit={(formEvent: FormEvent<HTMLFormElement>) => {
-            handleFormSubmit(
-              formEvent,
-              event.title.toLowerCase() as Event,
-              events,
-              setEvents,
-            );
-          }}
-        >
-          <EventFormElements
-            eventType={event.title.toLowerCase() as Event}
-            players={players}
-          />
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-          <div className="flex gap-2">
-            <Button type="submit">Submit</Button>
-          </div>
-        </Form>
-      </Popover.Dialog>
-    </Popover.Content>
-  </Popover>
-);
+  return (
+    <Popover isOpen={isOpen}>
+      <Button
+        onClick={() => setIsOpen(true)}
+        className={event.color}
+        fullWidth
+        size="lg"
+      >
+        {event.title}
+      </Button>
+      <Popover.Content className="max-w-72 overflow-scroll">
+        <Popover.Dialog>
+          <Popover.Heading className=" font-bold">
+            {event.addEvent}
+          </Popover.Heading>
+          <Form
+            className="flex flex-col gap-4"
+            onSubmit={(formEvent: FormEvent<HTMLFormElement>) => {
+              handleFormSubmit(
+                formEvent,
+                event.title.toLowerCase() as Event,
+                events,
+                setEvents,
+              );
+            }}
+          >
+            <EventFormElements
+              eventType={event.title.toLowerCase() as Event}
+              players={players}
+            />
+
+            <div className="flex gap-2">
+              <Button type="submit" onClick={() => setIsOpen(false)}>
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </Popover.Dialog>
+      </Popover.Content>
+    </Popover>
+  );
+};
